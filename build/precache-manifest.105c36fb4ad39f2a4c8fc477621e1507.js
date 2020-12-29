a154149f88412b85e13b1b3eb855c008a2d2997d
kritish bhattarai <rishi58rishi@gmail.com>
2020-12-29 12:25:20 +0545

    udpate

---
+++ build/precache-manifest.105c36fb4ad39f2a4c8fc477621e1507.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd69f4f653ba42e3f809b0d7b027fc37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174c30de837480cf14be",
+    "url": "/static/css/main.2273baec.chunk.css"
+  },
+  {
+    "revision": "e847a0cee073b5187f9a",
+    "url": "/static/js/2.66557be3.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.66557be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174c30de837480cf14be",
+    "url": "/static/js/main.101e419f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "58ca0c4cb74aecbd545da90c67b002ac",
+    "url": "/static/media/css.58ca0c4c.jpg"
+  },
+  {
+    "revision": "d911fadd299d18a2935abb66f8b87d70",
+    "url": "/static/media/mongodb.d911fadd.jpg"
+  }
+]);

1221a2174f0cd9bb6c5e58fd37ac0dd708f1bebc
kritish bhattarai <rishi58rishi@gmail.com>
2020-12-31 11:43:01 +0545

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9c63ca780e2025a24275ea4ff2ac9d0.js"
+  "/precache-manifest.c0a2d10d9efd811a6e81e379df76a35a.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/routes/Contact.js
+++ src/components/routes/Contact.js
@@ -3,74 +3,79 @@ import { motion } from 'framer-motion';
 import '../../css/contact.css';
 
 function Contact() {
-  return (
-    <div
-      className="d-flex flex-column justify-content-center px-5 pt-0 pb-5"
-      style={{ backgroundColor: '#1D1D1D', minHeight: '100vh', fontFamily: 'Poppins' }}
-    >
-      <motion.h1
-        initial={{ scale: 0, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1, transition: { duration: 0.3 } }}
-        style={{ cursor: 'default', color: '#ccc' }}
-        className=" skills-title mb-4"
-      >
-        contact
-      </motion.h1>
-      <motion.p
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 0.3, delay: 0.3 } }}
-        className=""
-        style={{ color: '#aaa', letterSpacing: 1, lineHeight: 1.6 }}
-      >
-        <p style={{ fontSize: 20, color: '#ccc' }}>you can reach me through</p>
-        <div>
-          <a
+    return (
+        <div
+            className="d-flex flex-column justify-content-center px-5 pt-0 pb-5"
+            style={{ backgroundColor: '#1D1D1D', minHeight: '100vh', fontFamily: 'Poppins' }}
+        >
+            <motion.h1
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1, transition: { duration: 0.3 } }}
+                style={{ cursor: 'default', color: '#ccc' }}
+                className=" skills-title mb-4"
+            >
+                contact
+            </motion.h1>
+            <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, transition: { duration: 0.3, delay: 0.3 } }}
+                className=""
+                style={{ color: '#aaa', letterSpacing: 1, lineHeight: 1.6 }}
+            >
+                <p style={{ fontSize: 20, color: '#ccc' }}>you can reach me through</p>
+                <div>
+                    {/* <a
             href="https://www.facebook.com/kritish.bhattarai.5"
             target="_blank"
             rel="noopener noreferrer"
             style={{ color: '#ddd' }}
           >
             <i style={{ fontSize: 24 }} className="fab fa-facebook-square mx-1"></i>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/rishi-rishi-a9b99a1a9/"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: '#ddd' }}
-          >
-            <i style={{ fontSize: 24 }} className="fab fa-linkedin mx-1"></i>
-          </a>
-          <div className="mt-1">rishi58rishi@gmail.com</div>
-          <>+977 9867461150</>
-        </div>
+          </a> */}
+                    <a
+                        href="https://www.linkedin.com/in/rishi-rishi-a9b99a1a9/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ color: '#ddd' }}
+                    >
+                        <i style={{ fontSize: 24 }} className="fab fa-linkedin mx-1"></i>
+                    </a>
+                    <div className="mt-1">rishi58rishi@gmail.com</div>
+                    <>+977 9867461150</>
+                </div>
 
-        <div className="mt-4">
-          <p style={{ fontSize: 20, color: '#ccc' }}>profile</p>
-          <div>
-            <a href="https://github.com/Kritish58" target="_blank" rel="noopener noreferrer" style={{ color: '#ddd' }}>
-              <i style={{ fontSize: 24 }} className="fab fa-github-square mx-1"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/rishi-rishi-a9b99a1a9/"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: '#ddd' }}
-            >
-              <i style={{ fontSize: 24 }} className="fab fa-linkedin mx-1"></i>
-            </a>
-            <a
-              href="https://www.facebook.com/kritish.bhattarai.5"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: '#ddd' }}
-            >
-              <i style={{ fontSize: 24 }} className="fab fa-facebook-square mx-1"></i>
-            </a>
-          </div>
+                <div className="mt-4">
+                    <p style={{ fontSize: 20, color: '#ccc' }}>profile</p>
+                    <div>
+                        <a
+                            href="https://github.com/Kritish58"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{ color: '#ddd' }}
+                        >
+                            <i style={{ fontSize: 24 }} className="fab fa-github-square mx-1"></i>
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/rishi-rishi-a9b99a1a9/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{ color: '#ddd' }}
+                        >
+                            <i style={{ fontSize: 24 }} className="fab fa-linkedin mx-1"></i>
+                        </a>
+                        {/* <a
+                            href="https://www.facebook.com/kritish.bhattarai.5"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{ color: '#ddd' }}
+                        >
+                            <i style={{ fontSize: 24 }} className="fab fa-facebook-square mx-1"></i>
+                        </a> */}
+                    </div>
+                </div>
+            </motion.p>
         </div>
-      </motion.p>
-    </div>
-  );
+    );
 }
 
 export default Contact;
